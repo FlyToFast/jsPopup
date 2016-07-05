bdfd4110d54df00ea178f0afb750cebb40bfbb64
Bram Regelbrugge <bram.regelbrugge@ucast.be>
2016-07-05 23:20:32 +0200

    added slider input and implemented example

--- jspopup.js
+++ jspopup.js
@@ -10,41 +10,56 @@ var jspopup = {
 	    jsPopupDiv += '<div class="jsp" id="jsp'+guid+'">';
 	    jsPopupDiv += '<div class="jspTopBar">';
 	    jsPopupDiv += '<div class=" jspPopupTitle">'+title+'</div>';
-        jsPopupDiv += '<div class="jspClose" id="popUpClose' + guid + '">';
+        jsPopupDiv += '<div class="jspClose" id="popUpClose' + guid + '"></div>';
         jsPopupDiv += '</div>';
-        jsPopupDiv += '</div>';
-
-        //     jsPopupDiv += '<div class="mfPopupLabelInput">';
-		// 	jsPopupDiv += '<label class="mfPopupLabel">ical-link</label>';
-		// 	jsPopupDiv += '<input class="mfPopupInput" type="text" id="icalLink'+guid+'" value="3">';
-        //     jsPopupDiv += '</div>';
-        
-        // jsPopupDiv += '<div class="mfPopupLabelInput">';
-		// 	jsPopupDiv += '<label class="mfPopupLabel">ical-link</label>';
-		// 	jsPopupDiv += '<input class="mfPopupInput" type="text" id="icalLink'+guid+'" value="3">';
-        //     jsPopupDiv += '</div>';
-        
-        // jsPopupDiv += '<div class="mfPopupLabelInput">';
-		// 	jsPopupDiv += '<label class="mfPopupLabel">ical-link</label>';
-		// 	jsPopupDiv += '<input class="mfPopupInput" type="text" id="icalLink'+guid+'" value="3">';
-        //     jsPopupDiv += '</div>';
-        
+        jsPopupDiv += '<div class="jspSettings"></div>';
         jsPopupDiv += '</div>';
         jsPopupDiv += '<div class="jspFullscreenDark" id="black' + guid + '"></div>';
         $('body').append(jsPopupDiv);
         this.closePopupListenet(guid);
+        $('#jsp' + guid).draggable(
+			{ handle: ".jspPopupTitle" }
+        );
+        return guid;
     },
     closePopupListenet: function (guid) {
         $('#popUpClose' + guid + ', #black' + guid).on('click', function () {
-            $('.jspf' + guid).remove();
+            $('#jspf' + guid).remove();
             $('#black' + guid).remove();
         });
+    },
+    addSetting: function (guid) {
+        var jsSetting = '';
+        jsSetting += '<div class="mfPopupLabelInput">';
+		jsSetting += '<label class="mfPopupLabel">attendees - Size</label>';
+		jsSetting += '<div class="mfPopupSlider" type="text" id="AttendeesSize'+guid+'"></div>';
+		jsSetting += '</div>';
+        $('#jsp' + guid).append(jsSetting);
+    },
+
+    addSliderSetting: function (guid, label, current, min, max, step, callback) {
+        var settingGuid = this.guid();
+        var jsSetting = '';
+        jsSetting += '<div class="jspSetting" id="jspSetting'+settingGuid+'">';
+        jsSetting += '<div class="jspLabel">'+label+'</div>';
+        jsSetting += '<div class="jspInput">';
+		jsSetting += '</div>';
+        $('#jsp' + guid + ' .jspSettings').append(jsSetting);
+        $('#jspSetting'+settingGuid+' .jspInput').slider({
+				range: "min",
+				max: max,
+				min: min,
+				value: current,
+				step: step,
+                slide: function (event, ui) {
+					callback(ui.value);
+				}
+			});
     },
     guid: function () {
         function s4() {
             return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
+            .toString(16).substring(1);
         }
         return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
